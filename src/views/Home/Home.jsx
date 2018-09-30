5dd4c169f68da0b1d185d2aac74700706f2297a9
nadun94 <nadunsirimevan@gmail.com>
2018-09-30 11:02:00 +0530

    adf

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -49,12 +49,7 @@ class Home extends React.Component {
                           </div>
                         </div>
                       </Slide>
-                      <div><h6>We are a leading non-state degree awarding institute approved by the University Grants Commission
-                      (UGC) under the Universities Act. We are also members of the Association of Commonwealth Universities (ACU), as well as the International Association of Universities (IAU), and the first Sri Lankan institute to be accredited by the Institution of Engineering & Technology, UK.
-                
-We are proud to be listed as a leading and formidable awarding institute authorised and approved by the University 
-Grants Commission (UGC) under the Universities Act, and the International Association of Universities (IAU). Furthermore, 
-not only are we the first Sri Lankan institute to be accredited by the Institution of Engineering & Technology (IET.), UK, our IT degrees are also in turn accredited by the Engineering Council, UK</h6></div>
+                     
                   </div>
                     </div>  
               {/* <Card className="card-chart">
